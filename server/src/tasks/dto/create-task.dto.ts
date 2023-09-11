909d5d483b3dca4157fa7037f9993751a882f094
Kateryna Kleshch <ketrins1993@gmail.com>
2023-09-11 20:25:45 +0300

    add validator

--- server/src/tasks/dto/create-task.dto.ts
+++ server/src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,12 @@
-export class CreateTaskDto {}
+import { IsString, IsDate } from 'class-validator'
+
+export class CreateTaskDto {
+  @IsString()
+  name: string
+
+  @IsDate()
+  dateStart: Date
+
+  @IsDate()
+  dateEnd: Date
+}
--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsEmail, MaxLength, MinLength } from "class-validator"
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string
+
+  @MinLength(6, {
+    message: 'Password is too short',
+  })
+  @MaxLength(10, {
+    message: 'Password is too long',
+  })
+  password: string
+}
