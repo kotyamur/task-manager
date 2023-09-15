9ba2ce84ce9d5344e2625751c0217d683a972c4a
Kateryna Kleshch <ketrins1993@gmail.com>
2023-09-16 01:28:37 +0300

    add datepickers

--- client/src/components/Task/Task.tsx
+++ client/src/components/Task/Task.tsx
@@ -1,4 +1,9 @@
 import * as React from "react";
+
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
 // import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -7,8 +12,10 @@ import Button from "@mui/material/Button";
 
 const Task: React.FC = () => {
     const [name, setName] = React.useState("");
-
     const [description, setDescription] = React.useState("");
+    const [startDate, setStartDate] = React.useState<dateFns | null>(null);
+    const [endDate, setEndDate] = React.useState<dateFns | null>(null);
+    console.log(startDate, endDate);
 
   return (
     <Box
@@ -22,16 +29,17 @@ const Task: React.FC = () => {
       <Box
         component="form"
         sx={{
-          "& .MuiTextField-root": { m: 1, width: "30ch" },
           padding: 4,
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          width: "35ch",
         }}
         noValidate
         autoComplete="off"
       >
         <TextField
+          sx={{ width: "35ch", mb: 1 }}
           label="Name:"
           value={name}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,6 +47,7 @@ const Task: React.FC = () => {
           }}
         />
         <TextField
+          sx={{ width: "35ch" }}
           id="outlined-multiline-static"
           label="Description"
           multiline
@@ -48,14 +57,55 @@ const Task: React.FC = () => {
             setDescription(event.target.value);
           }}
         />
-        <Box sx={{ display: "flex", gap: 6, justifyContent: "center", mt: 3 }}>
-          <Typography variant="body1">start date</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            mt: 3,
+          }}
+        >
+          <Typography variant="body1" sx={{ width: "80px" }}>
+            start date
+          </Typography>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              sx={{
+                width: "170px",
+              }}
+              value={startDate}
+              onChange={(newValue) => setStartDate(newValue)}
+              //   defaultValue={format(new Date())}
+            />
+          </LocalizationProvider>
         </Box>
-        <Box sx={{ display: "flex", gap: 6, justifyContent: "center", mt: 3 }}>
-          <Typography variant="body1">end date</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            mt: 3,
+          }}
+        >
+          <Typography variant="body1" sx={{ width: "80px" }}>
+            end date
+          </Typography>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              sx={{
+                width: "170px",
+              }}
+              value={endDate}
+              onChange={(newValue) => setEndDate(newValue)}
+              minDate={startDate}
+              //   defaultValue={format(new Date())}
+            />
+          </LocalizationProvider>
         </Box>
 
-        <Box sx={{ display: "flex", gap: 6, justifyContent: "center", mt: 3 }}>
+        <Box sx={{ display: "flex", gap: 13, mt: 3 }}>
           <Button variant="outlined">cancel</Button>
           <Button variant="contained">save</Button>
         </Box>
